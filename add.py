447b425a58a4053074b3a8825b0246c260e6d107
nazar200215 <nazar.tovstenko02@gmail.com>
2019-09-22 21:42:05 +0300

    .

--- add.py
+++ add.py
@@ -2,8 +2,6 @@
 def main():
     a = float(input("перше число ="))
     b = float(input("друге число ="))
-    if a == b:
-        print(f'a=b, перемножаєм числа ={a*a}')
-    else:
-        print(f'сума ={a+b}')
+
+    print(f'{a} + {b} = {a+b}')
 if __name__ == '__main__': main()
