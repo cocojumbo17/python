3cf2d15f3d0acadd5ed6268108f73c57678b04ec
nazar200215 <nazar.tovstenko02@gmail.com>
2019-09-25 21:17:10 +0300

    11

--- pifagor.py
+++ pifagor.py
@@ -1,8 +1,8 @@
-n = int(input('Введіть число від 1 до 20: '))
-if n > 20 or n < 1:
+m = int(input('Введіть число від 1 до 20: '))
+if m > 20 or m < 1:
     print("число не є вірним")
 else:
-    for row in range(1, n+1):
-        for col in range(1, n+1):
+    for row in range(1, m+1):
+        for col in range(1, m+1):
             print(row * col, end='\t')
         print(" ")
