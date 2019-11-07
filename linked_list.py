9a982f4f3a14b8190d6c2a05abf99fee6510c170
nazar200215 <nazar200215@ex.ua>
2019-11-07 21:08:31 +0200

    parsing of linked list

--- linked_list.py
+++ linked_list.py
@@ -1,10 +1,3 @@
-import math
-import os
-import random
-import re
-import sys
-
-
 class Node:
     def __init__(self, value, next=None, prev=None):
         self._value = float(value)
@@ -148,6 +141,76 @@ class LinkedList:
             curr_node = curr_node.GetPrev()
         print()
 
+def parseCommand(command):
+    """
+    parse command in string to binary format
+    :param command: string
+    :return: list of:
+    elem0 - boolean flag (true - no error, false - error of format)
+    elem1 - command (1 - push, 2 - pop, 3 - unshift, 4 - shift, 5 - insert)
+    elem2 - optional param of functions (push - float element, unshift - float element, insert - int index)
+    elem3 - optional param of functions (insert - float element)
+    """
+    parts = command.split()
+    if len(parts) == 0:
+        return [False]
+    if parts[0] == 'push':
+        if len(parts) != 2:
+            return [False]
+        el = float(parts[1])
+        return [True, 1, el]
+    if parts[0] == 'pop':
+        return [True, 2]
+    if parts[0] == 'unshift':
+        if len(parts) != 2:
+            return [False]
+        el = float(parts[1])
+        return [True, 3, el]
+    if parts[0] == 'shift':
+        return [True, 4]
+    if parts[0] == 'insert':
+        if len(parts) != 3:
+            return [False]
+        index = int(parts[1])
+        el = float(parts[2])
+        return [True, 5, index, el]
+    return [False]
+
+
+def executeCommand(command, ll):
+    if command[1] == 1:
+        ll.push(command[2])
+    elif command[1] == 2:
+        ll.pop()
+    elif command[1] == 3:
+        ll.unshift(command[2])
+    elif command[1] == 4:
+        ll.shift()
+    elif command[1] == 5:
+        ll.insert(command[2], command[3])
 
 def main():
-   
+    N = int(input('Number of elements N='))
+    str_elements = input('Elements are:')
+    all_elements = str_elements.split()
+    if len(all_elements) != N:
+        print('Incorrect number of elements')
+        return
+    our_list = LinkedList()
+    for el in all_elements:
+        our_list.push(float(el))
+
+    M = int(input('Number of commands M='))
+    for i in range(M):
+        str_command = input(f'Command #{i}:')
+        bin_command = parseCommand(str_command)
+        if bin_command[0]:
+            executeCommand(bin_command, our_list)
+        else:
+            print("Wrong command's format")
+            return
+    our_list.print_r()
+    our_list.print()
+
+
+main()
